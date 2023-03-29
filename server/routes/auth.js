ba9a70cbc80624e5298bac2a3087dac4381b8b35
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-03-29 22:57:30 +0300

    user registration and login with validation

---
+++ server/routes/auth.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable camelcase */
+/* eslint-disable consistent-return */
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const {
+  registerValidation,
+  loginValidation,
+} = require('../validation/validation');
+
+const tokenSecret = process.env.TOKEN_SECRET;
+
+router.post('/register', async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send('Email already exists');
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email does not exist');
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Incorrect password');
+
+  const token = jwt.sign({ _id: user._id }, tokenSecret);
+  res.header('auth-token', token).send(token);
+});
+
+module.exports = router;
