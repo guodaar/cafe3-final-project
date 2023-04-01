bc84efe8b7a26097fb211b991f326aa561670656
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-01 19:34:20 +0300

    created hero for main page

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,10 +1,17 @@
+import { eggshell, lavender, white } from "../consts/colors";
+
+import Emoji from "../components/Emoji/Emoji";
 import { PropsWithChildren } from "react";
-import { eggshell } from "../consts/colors";
 import styled from "styled-components";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
     <MainContainer>
+      <HeroContainer>
+        <HeroText>
+          Igniting your curiosity with every <span>spark</span>
+        </HeroText>
+      </HeroContainer>
       <Content>{children}</Content>
     </MainContainer>
   );
@@ -14,12 +21,29 @@ export default AuthLayout;
 
 const MainContainer = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: space-between;
+  align-items: center;
   height: 100vh;
+  margin: 0 10vw;
 `;
 
 const Content = styled.div`
   flex: 1;
   background-color: ${eggshell};
+`;
+
+const HeroContainer = styled.div`
+  flex: 1.5;
+`;
+
+const HeroText = styled.h1`
+  font-family: "Montserrat";
+  font-size: 4rem;
+  line-height: 1.2;
+
+  span {
+    background-color: ${lavender};
+    color: ${white};
+    padding: 0 18px 5px;
+  }
 `;
