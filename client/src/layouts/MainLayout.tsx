cf4e8f60ea2cf6a3d1bc45e96484f5dda4300bc9
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-01 19:34:49 +0300

    styled main layout with topbar and sidebar

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,4 +1,7 @@
 import { PropsWithChildren } from "react";
+import SideBar from "./SideBar";
+import TopBar from "./TopBar";
+import { eggshell } from "../consts/colors";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
@@ -6,7 +9,13 @@ const MainLayout = ({ children }: PropsWithChildren) => {
   const navigate = useNavigate();
   return (
     <MainContainer>
-      <Content>{children}</Content>
+      <TopBar></TopBar>
+      <Content>
+        <Left>
+          <SideBar />
+        </Left>
+        <Right>{children}</Right>
+      </Content>
     </MainContainer>
   );
 };
@@ -17,11 +26,21 @@ const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-color: ${eggshell};
   height: 100vh;
 `;
 
 const Content = styled.div`
+  padding: 54px 5vw;
+  display: flex;
+  justify-content: space-between;
+  gap: 3vw;
+`;
+
+const Left = styled.div`
   flex: 1;
-  background-color: #f1efef;
-  padding: 32px 15vw;
+`;
+
+const Right = styled.div`
+  flex: 3;
 `;
