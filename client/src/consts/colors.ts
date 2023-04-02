bfd64044c03aa4ea50140ab5fee8f82c6670d53d
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-02 19:44:52 +0300

    adjusted style and colors values

--- client/src/consts/colors.ts
+++ client/src/consts/colors.ts
@@ -1,7 +1,7 @@
 export const black = "rgb(34,35,37)";
 export const lavender = "rgb(148,156,255)";
 export const orange = "rgb(252,117,87)";
-export const eggshell = "rgb(247,245,237)";
+export const eggshell = "#f7f5ed";
 export const yellow = "rgb(255,199,58)";
-export const lightGrey = "rgb(244,244,244)";
-export const white = "rgb(248,249,249)";
+export const lightGrey = "rgb(240,240,240)";
+export const white = "#ffffff";
--- client/src/consts/style.ts
+++ client/src/consts/style.ts
@@ -1,6 +1,8 @@
 export const border = "1.5px solid black";
-export const boxShadow = "6px 6px 0 -1px rgb(34,35,37);";
-export const reverseBoxShadow = "-6px 6px 0 -1px rgb(34,35,37);";
 export const borderRadius = "10px";
 export const roundedBorder = "25px";
-export const transition = "0.2s ease-in-out;";
+export const transition = "0.2s ease-in-out";
+export const clickedTransform = "translate(-4px, 4px)";
+
+export const boxShadow = "-6px 8px 0 -1px rgb(34,35,37)";
+export const clickedBoxShadow = "-2px 2px 0 0 rgb(34,35,37)";
