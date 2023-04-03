0ed46264eb1bb52ef46092f04382f9133dde8417
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-03 23:00:58 +0300

    style const adjustments and font weight

--- client/src/consts/style.ts
+++ client/src/consts/style.ts
@@ -1,7 +1,7 @@
 export const border = "1.5px solid black";
 export const borderRadius = "10px";
 export const roundedBorder = "25px";
-export const transition = "0.2s ease-in-out";
+export const transition = "0.1s ease-in-out";
 export const clickedTransform = "translate(-4px, 4px)";
 
 export const boxShadow = "-6px 8px 0 -1px rgb(34,35,37)";
