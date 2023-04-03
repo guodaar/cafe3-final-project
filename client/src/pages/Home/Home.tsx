e1c5b13f1cf23fdb9a0108b6eb94fb32201513b5
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-03 23:07:17 +0300

    fixed error

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,15 +2,15 @@ import { lavender, white } from "../../consts/colors";
 
 import { Question } from "../../types/question";
 import QuestionCard from "../../components/QuestionCard/QuestionCard";
+import { UserContext } from "../../contexts/userContext";
 import { roundedBorder } from "../../consts/style";
 import styled from "styled-components";
+import { useContext } from "react";
 import { useQuestions } from "../../hooks/questions";
-import { useSessionStorage } from "../../hooks/sessionStorage";
 
-type Props = {};
-
-const Home = (props: Props) => {
-  const [token] = useSessionStorage<string>("token", "");
+const Home = () => {
+  const { user } = useContext(UserContext);
+  const token = user?.token ?? "";
   const { data, isLoading } = useQuestions(token);
   const questions = data || [];
   console.log(questions);
@@ -34,8 +34,7 @@ export default Home;
 const Content = styled.div`
   background-color: ${lavender};
   border-radius: ${roundedBorder};
-  height: 100vh;
-  padding: 32px 5vw;
+  padding: 32px 5vw 64px;
 `;
 
 const Title = styled.h2`
