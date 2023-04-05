1d2b5d03b7ff448617b15c9e9ed5543a84109ec0
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-05 22:52:40 +0300

    styled toast messages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { border, roundedBorder } from "./consts/style";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -8,10 +9,6 @@ import Modal from "react-modal";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement
-// );
 
 const queryClient = new QueryClient();
 Modal.setAppElement("#root");
@@ -22,7 +19,17 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-      <Toaster position="bottom-center" />
+      <Toaster
+        position="bottom-left"
+        toastOptions={{
+          className: "",
+          style: {
+            border: border,
+            padding: "8px",
+            borderRadius: roundedBorder,
+          },
+        }}
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
