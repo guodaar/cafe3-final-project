7bb19cef767096ac5a3d91e0fb07c4602ef62f22
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-04-05 22:51:12 +0300

    formik textarea

--- client/src/components/Formik/FormikTextarea.tsx
+++ client/src/components/Formik/FormikTextarea.tsx
@@ -1,37 +1,33 @@
 import { ErrorMessage, Field } from "formik";
-import { black, orange } from "../../consts/colors";
-import { border, roundedBorder } from "../../consts/style";
 
+import Input from "../Input/Input";
 import { InputHTMLAttributes } from "react";
+import { orange } from "../../consts/colors";
 import styled from "styled-components";
 
 type Props = {
   name: string;
   type?: InputHTMLAttributes<HTMLInputElement>["type"];
   placeholder?: InputHTMLAttributes<HTMLInputElement>["placeholder"];
+  value?: string;
 };
 
 const FormikTextArea = ({ name, ...restProps }: Props) => {
   return (
     <div>
-      <Field name={name} as={StyledArea} {...restProps} />
+      <Field
+        name={name}
+        as={Input}
+        variant="textarea"
+        {...restProps}
+        rows={6}
+      />
       <Error name={name} component="div" />
     </div>
   );
 };
 
 export default FormikTextArea;
-
-const StyledArea = styled.textarea`
-  font-size: 16px;
-  color: ${black};
-  background-color: white;
-  padding: 12px 24px;
-  border: ${border};
-  border-radius: ${roundedBorder};
-  outline: none;
-  width: 100%;
-`;
 
 const Error = styled(ErrorMessage)`
   font-size: 0.9rem;
